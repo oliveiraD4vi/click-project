57251106e21c8dd915ac5bc6126ea83201d0bfbb
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-06-22 13:56:22 -0300

    [HU-07] login form added

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,9 +1,110 @@
+import { useState } from 'react';
+import { LockOutlined, FieldNumberOutlined } from '@ant-design/icons';
+import { Form, Input, Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+import { Notification, auth } from '../../services/utils';
+import api from '../../services/api';
+
 import './login.scss';
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+
+  const handleSubmit = async (values) => {
+    const { matricula, password } = values;
+
+    setLoading(true);
+    setDisabled(true);
+
+    try {
+      const response = await api.post('/login', {
+        matricula,
+        password,
+      });
+
+      const { data } = response;
+      auth.login(data.authData);
+      
+      Notification('success', data.message);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setDisabled(false);
+
+      const { data } = error.response;
+      Notification('error', data.message);
+    }
+  }
+
   return (
     <div className="login-container">
-      login
+      <div className="login-box">
+        <div className="login-box-in">
+          <Form form={form} className="login-form" onFinish={handleSubmit}>
+            <Form.Item
+              name="matricula"
+              rules={[
+                {
+                  required: true,
+                  message: 'Digite sua matrícula',
+                },
+                {
+                  min: 6,
+                  message: 'Essa matrícula não é válida',
+                },
+                {
+                  max: 6,
+                  message: 'Essa matrícula não é válida',
+                },
+              ]}
+            >
+              <Input
+                disabled={disabled}
+                prefix={<FieldNumberOutlined className="icon" />}
+                placeholder="Matrícula"
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              className="input-password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Digite sua senha',
+                },
+                {
+                  min: 8,
+                  message: 'A senha deve ter, no mínimo, 8 caracteres',
+                },
+              ]}
+            >
+              <Input
+                disabled={disabled}
+                placeholder="Senha"
+                type="password"
+                prefix={<LockOutlined className="icon" />}
+              />
+            </Form.Item>
+
+            <Form.Item className="btn">
+              <Button
+                loading={loading}
+                type="primary"
+                htmlType="submit"
+                className="primary-button"
+              >
+                ENTRAR
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
