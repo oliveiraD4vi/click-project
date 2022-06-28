1ef4ae8f74c0af614ed11b1cb5beea4d6f00c686
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-06-27 23:50:05 -0300

    [ID-12] added about and landing page

--- src/screens/LandingPage/landingPage.js
+++ src/screens/LandingPage/landingPage.js
@@ -1,9 +1,44 @@
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import Transparency from '../../assets/landing-transparency.svg';
+
 import './landingPage.scss';
 
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="landing-container">
-      <h1>Landing</h1>
+      <div className="left-container">
+        <h2>Seja Bem Vindo!</h2>
+        <p>
+          Olá clicker! Esse é um site para votação de filmes que serão exibidos no cineEscola.
+          Para votar no seu filme preferido faça login ou cadastre-se.
+        </p>
+
+        <div className="btn-container">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="primary-button"
+            onClick={() => navigate('/login')}
+          >
+            LOGIN
+          </Button>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="secondary-button"
+            onClick={() => navigate('/register')}
+          >
+            CADASTRAR
+          </Button>
+        </div>
+      </div>
+      <div className="right-container">
+        <img src={Transparency} alt="landing image" />
+      </div>
     </div>
   );
 }
